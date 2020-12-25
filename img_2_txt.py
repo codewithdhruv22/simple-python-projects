ed262dc208296ab8bd9e09d29eed3c8374f84141
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2020-12-25 09:10:25 +0530

    Convert Image Into ASCII Art using python
    Watch the tutorial of this code on my YouTube channel
    Here - https://www.youtube.com/channel/UCmOBuijDvNgUMlqpzrwEBcw

---
+++ img_2_txt.py
@@ -0,0 +1,7 @@
+#Convert Image Into ASCII Art
+
+import pywhatkit
+pywhatkit.image_to_ascii_art("img path","converted txt path")
+
+# Watch the tutorial of this code on my youtube channel
+# Here - https://www.youtube.com/channel/UCmOBuijDvNgUMlqpzrwEBcw
