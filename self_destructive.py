9469c3f1f9b705e7e3a91ac3e435ac86ddee12fb
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2020-12-27 14:44:50 +0530

    Self Destructive Python Program
    To Learn More About This Code
    Visit My YouTube Channel - https://www.youtube.com/channel/UCmOBuijDvNgUMlqpzrwEBcw

---
+++ self_destructive.py
@@ -0,0 +1,8 @@
+# Self Destructive Python Program
+
+import os, sys
+
+os.remove(sys.argv[0])
+
+# To Learn More About This Code
+# Visit My Youtube Channel - https://www.youtube.com/channel/UCmOBuijDvNgUMlqpzrwEBcw
